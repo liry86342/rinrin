763a1306a4253b27d51d76fbde5aef76370dee62
liry86342 <202017027@ryugasaki.ed.jp>
2024-12-17 12:21:53 +0900

    Update proxyExtension.js

--- proxyExtension.js
+++ proxyExtension.js
@@ -1,1 +1,35 @@
+class ProxyExtension {
+  getInfo() {
+    return {
+      id: 'proxy',
+      name: 'Proxy Extension',
+      blocks: [
+        {
+          opcode: 'fetchFromProxy',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'fetch from proxy [URL]',
+          arguments: {
+            URL: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'https://example.com'
+            }
+          }
+        }
+      ]
+    };
+  }
 
+  fetchFromProxy(args) {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    const finalUrl = proxyUrl + args.URL;
+    
+    return new Promise((resolve, reject) => {
+      fetch(finalUrl)
+        .then(response => response.ok ? response.text() : Promise.reject('Failed to fetch'))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+}
+
+scratch.extensions.register(new ProxyExtension());
